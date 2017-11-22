bd562cb64cdc1eb27f315b521aeaab27c00533e4
Jack Chapman <jackchapman1997@hotmail.co.uk>
2017-11-22 14:18:42 +0000

    added comment to previousButton listener

--- app/src/main/java/com/example/c3451748/todo/TodoActivity.java
+++ app/src/main/java/com/example/c3451748/todo/TodoActivity.java
@@ -84,6 +84,8 @@ public class TodoActivity extends AppCompatActivity {
         buttonPrev.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                
+//              conditional operator "?" used to evaluate if the expression in the brackets is true or false.
                 mTodoIndex = (--mTodoIndex < 0) ? mTodos.length-1 : mTodoIndex;
                 textViewTodo.setText(mTodos[mTodoIndex]);
             }
